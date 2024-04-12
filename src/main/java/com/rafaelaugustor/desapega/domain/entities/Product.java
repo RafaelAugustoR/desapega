0f7f77084a25bb22cfc03e56ea1100d0b0e3f652
Rafael <rafaelaugustodev@gmail.com>
2024-04-12 16:01:57 -0300

    feat: creating entity Product

---
+++ src/main/java/com/rafaelaugustor/desapega/domain/entities/Product.java
@@ -0,0 +1,26 @@
+package com.rafaelaugustor.desapega.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID uuid;
+
+    private String name;
+
+    private String description;
+
+    private String image;
+
+}
