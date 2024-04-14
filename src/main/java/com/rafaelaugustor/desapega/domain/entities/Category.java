b8b900ca8e5e3709c3e3c1ff192a2d4686a27fcd
Rafael <rafaelaugustodev@gmail.com>
2024-04-13 22:32:34 -0300

    feat: creating entity Category

---
+++ src/main/java/com/rafaelaugustor/desapega/domain/entities/Category.java
@@ -0,0 +1,27 @@
+package com.rafaelaugustor.desapega.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID uuid;
+
+    private String name;
+
+    private String description;
+
+    private String image;
+
+}
