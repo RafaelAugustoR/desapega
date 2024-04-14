99ee1e9721aaa4252be72590c32958736a80f19a
Rafael <rafaelaugustodev@gmail.com>
2024-04-14 15:21:00 -0300

    feat: adding new field for set status of the transaction

--- src/main/java/com/rafaelaugustor/desapega/domain/entities/Transaction.java
+++ src/main/java/com/rafaelaugustor/desapega/domain/entities/Transaction.java
@@ -1,11 +1,13 @@
 package com.rafaelaugustor.desapega.domain.entities;
 
+import com.rafaelaugustor.desapega.domain.enums.TransactionStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -18,11 +20,9 @@ public class Transaction {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID uuid;
 
-    private String name;
-
-    private String description;
-
     private Instant date;
+
+    private TransactionStatus status;
 
     @ManyToOne
     private User requester;
