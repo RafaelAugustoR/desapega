57ef83b5f27e6f21d0e8ab69492f932b70364634
Rafael <rafaelaugustodev@gmail.com>
2024-04-13 22:34:51 -0300

    feat: creating entity Evaluation

---
+++ src/main/java/com/rafaelaugustor/desapega/domain/entities/Evaluation.java
@@ -0,0 +1,27 @@
+package com.rafaelaugustor.desapega.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Evaluation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID uuid;
+
+    private Integer rating;
+
+    private String comment;
+
+    private Instant date;
+
+}
