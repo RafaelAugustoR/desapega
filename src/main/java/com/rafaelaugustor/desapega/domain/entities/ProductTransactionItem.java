b462e054c7e1a56dd8efeb83990bca62984bd05a
Rafael <rafaelaugustodev@gmail.com>
2024-04-14 15:12:01 -0300

    fix: creating entity ProductTransactionItem and adding relations with other entities

---
+++ src/main/java/com/rafaelaugustor/desapega/domain/entities/ProductTransactionItem.java
@@ -0,0 +1,25 @@
+package com.rafaelaugustor.desapega.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductTransactionItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID uuid;
+
+    @ManyToOne
+    private Transaction transaction;
+
+    @ManyToOne
+    private Product product;
+}
