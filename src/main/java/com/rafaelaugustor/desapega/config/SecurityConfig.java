1f59cbe8fc8dc3576d9ef352e5db37ec75f5895c
Rafael <rafaelaugustodev@gmail.com>
2024-04-21 19:37:11 -0300

    feat: configuring Spring Security for H2 Console in test environment

--- src/main/java/com/rafaelaugustor/desapega/config/SecurityConfig.java
+++ src/main/java/com/rafaelaugustor/desapega/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.rafaelaugustor.desapega.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,8 +14,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    @Profile("test")
+    @Order(1)
+    public SecurityFilterChain h2SecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.securityMatcher(PathRequest.toH2Console()).csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+        return httpSecurity.build();
+    }
 
     @Bean
+    @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
