c7c4ce13980e4c3d80678377a01f3f552cbcce3e
Rafael <rafaelaugustodev@gmail.com>
2024-04-21 19:09:45 -0300

    feat: implementing UserDetails interface in User entity

--- src/main/java/com/rafaelaugustor/desapega/domain/entities/User.java
+++ src/main/java/com/rafaelaugustor/desapega/domain/entities/User.java
@@ -3,8 +3,12 @@ package com.rafaelaugustor.desapega.domain.entities;
 import com.rafaelaugustor.desapega.domain.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,7 +18,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 @Builder
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -40,6 +44,9 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private UserRole role;
+
+    @OneToOne
+    private Address address;
 
     @OneToMany(mappedBy = "user")
     private List<Product> products;
@@ -68,6 +75,34 @@ public class User {
     @OneToMany(mappedBy = "senderSolicitation")
     private List<Notification> sentNotifications;
 
-    @OneToOne
-    private Address address;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
