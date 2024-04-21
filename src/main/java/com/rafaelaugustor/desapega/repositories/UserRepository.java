feb92b14f8383f748a9131f21278d2d8c0824306
Rafael <rafaelaugustodev@gmail.com>
2024-04-20 23:12:35 -0300

    feat: creating UserRepository

---
+++ src/main/java/com/rafaelaugustor/desapega/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.rafaelaugustor.desapega.repositories;
+
+import com.rafaelaugustor.desapega.domain.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
