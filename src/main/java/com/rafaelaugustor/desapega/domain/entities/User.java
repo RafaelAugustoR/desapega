c35fc32d5ca3cd47d9c35895b043d183d9f9349f
Rafael <rafaelaugustodev@gmail.com>
2024-04-21 19:06:23 -0300

    feat: adding EqualsAndHashCode to entity User

--- src/main/java/com/rafaelaugustor/desapega/domain/entities/User.java
+++ src/main/java/com/rafaelaugustor/desapega/domain/entities/User.java
@@ -1,10 +1,7 @@
 package com.rafaelaugustor.desapega.domain.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -14,6 +11,7 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 @Builder
 public class User {
 
