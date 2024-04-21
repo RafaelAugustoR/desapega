972839c95538aac87c541d524df4157901242b5e
Rafael <rafaelaugustodev@gmail.com>
2024-04-21 20:24:22 -0300

    feat: creating AuthService and implementing a method to register a new user

---
+++ src/main/java/com/rafaelaugustor/desapega/services/AuthService.java
@@ -0,0 +1,56 @@
+package com.rafaelaugustor.desapega.services;
+
+import com.rafaelaugustor.desapega.domain.entities.Address;
+import com.rafaelaugustor.desapega.domain.entities.User;
+import com.rafaelaugustor.desapega.domain.enums.UserRole;
+import com.rafaelaugustor.desapega.repositories.AddressRepository;
+import com.rafaelaugustor.desapega.repositories.UserRepository;
+import com.rafaelaugustor.desapega.rest.dtos.request.RegisterRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final AddressRepository addressRepository;
+
+
+    public void register(RegisterRequestDTO request){
+
+        Address userAddress = Address.builder()
+                .country(request.getAddress().getCountry())
+                .city(request.getAddress().getCity())
+                .state(request.getAddress().getState())
+                .neighborhood(request.getAddress().getNeighborhood())
+                .street(request.getAddress().getStreet())
+                .postalCode(request.getAddress().getPostalCode())
+                .houseNumber(request.getAddress().getHouseNumber())
+                .complement(request.getAddress().getComplement())
+                .build();
+
+        addressRepository.save(userAddress);
+
+        if (!request.getPassword().equals(request.getConfirmPassword())){
+            throw new RuntimeException();
+        }
+
+        User userToSave = User.builder()
+                .name(request.getName())
+                .cpf(request.getCpf())
+                .email(request.getEmail())
+                .birthDate(request.getBirthDate())
+                .address(userAddress)
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(UserRole.COSTUMER)
+                .build();
+
+        userRepository.save(userToSave);
+    }
+
+}
