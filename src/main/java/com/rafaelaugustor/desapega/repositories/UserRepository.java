cbfaeceab75ce318ab3d6e41b7a515e8d120d5ff
Rafael <rafaelaugustodev@gmail.com>
2024-04-23 18:44:30 -0300

    refactor: adding annotation @Repository to repositories

--- src/main/java/com/rafaelaugustor/desapega/repositories/UserRepository.java
+++ src/main/java/com/rafaelaugustor/desapega/repositories/UserRepository.java
@@ -2,10 +2,10 @@ package com.rafaelaugustor.desapega.repositories;
 
 import com.rafaelaugustor.desapega.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-
     User findByEmail(String email);
 }
