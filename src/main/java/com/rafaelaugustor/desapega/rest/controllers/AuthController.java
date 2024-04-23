059a0dd9bee71a95a4aaf9a4772ed0785912cb7e
Rafael <rafaelaugustodev@gmail.com>
2024-04-23 18:43:03 -0300

    feat: adding endpoint for user login

--- src/main/java/com/rafaelaugustor/desapega/rest/controllers/AuthController.java
+++ src/main/java/com/rafaelaugustor/desapega/rest/controllers/AuthController.java
@@ -1,10 +1,11 @@
 package com.rafaelaugustor.desapega.rest.controllers;
 
+import com.rafaelaugustor.desapega.rest.dtos.request.LoginRequestDTO;
 import com.rafaelaugustor.desapega.rest.dtos.request.RegisterRequestDTO;
+import com.rafaelaugustor.desapega.rest.dtos.response.LoginResponseDTO;
 import com.rafaelaugustor.desapega.services.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +18,13 @@ import static com.rafaelaugustor.desapega.utils.Constants.APP_ROOT;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-
     private final AuthService authService;
 
-
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO request) {
+        LoginResponseDTO response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
 
     @PostMapping("/register")
     public ResponseEntity<Void> register(@RequestBody RegisterRequestDTO request) {
