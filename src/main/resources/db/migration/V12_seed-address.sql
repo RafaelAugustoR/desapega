ffbec38bd70d52c3c4b0800524268f8ad50d5fcb
Rafael <rafaelaugustodev@gmail.com>
2024-04-23 20:29:08 -0300

    feat: creating migration seed table Address

---
+++ src/main/resources/db/migration/V12_seed-address.sql
@@ -0,0 +1,4 @@
+INSERT INTO address (id, country, city, state, neighborhood, street, postal_code, house_number, complement) VALUES
+('6ba7b810-9dad-11d1-80b4-00c04fd430c8', 'Brasil', 'Florianópolis', 'SC', 'Centro', 'Rua Principal', '88000000', '123', 'Apto 101'),
+('6ba7b811-9dad-11d1-80b4-00c04fd430c9', 'Brasil', 'Porto Alegre', 'RS', 'Centro', 'Rua da Praia', '90000000', '456', 'Casa'),
+('6ba7b812-9dad-11d1-80b4-00c04fd430ca', 'Brasil', 'São Paulo', 'SP', 'Jardins', 'Avenida Paulista', '01310000', '789', 'Condomínio');
