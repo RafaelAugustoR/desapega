040065b39806331619027370f3024c56f91bade5
Rafael <rafaelaugustodev@gmail.com>
2024-04-25 22:48:34 -0300

    feat: adding Builder to Category

--- src/main/java/com/rafaelaugustor/desapega/domain/entities/Category.java
+++ src/main/java/com/rafaelaugustor/desapega/domain/entities/Category.java
@@ -2,6 +2,7 @@ package com.rafaelaugustor.desapega.domain.entities;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +13,7 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Category {
 
     @Id
