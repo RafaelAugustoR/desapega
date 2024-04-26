60b089e7287f0a72177d0b5fcb1234ddccabe1c3
Rafael <rafaelaugustodev@gmail.com>
2024-04-26 15:33:25 -0300

    refactor: updating seeder for testing security

--- src/main/resources/db/migration/V13_seed-user.sql
+++ src/main/resources/db/migration/V13_seed-user.sql
@@ -1,5 +1,4 @@
-INSERT INTO "user" (id, name, cpf, birth_date, password, email, profile_picture, address_id) VALUES
-('6ba7b810-9dad-11d1-80b4-00c04fd430c2', 'João Silva', '123.456.719-08', '1990-01-01', 'senha123', 'joao@example.com', 'profile1.jpg', '6ba7b810-9dad-11d1-80b4-00c04fd430c8'),
-('6ba7b810-9dad-11d1-80b4-00c04fd430c1', 'Maria Santos', '123.456.719-01', '1985-05-15', 'senha456', 'maria@example.com', 'profile2.jpg', '6ba7b811-9dad-11d1-80b4-00c04fd430c9'),
-('6ba7b810-9dad-11d1-80b4-00c04fd430cb', 'Carlos Oliveira', '123.456.719-00', '1995-09-30', 'senha789', 'carlos@example.com', 'profile3.jpg', '6ba7b812-9dad-11d1-80b4-00c04fd430ca');
-
+INSERT INTO "user" (id, name, cpf, birth_date, password, email, profile_picture, address_id, role) VALUES
+('6ba7b810-9dad-11d1-80b4-00c04fd430c2', 'João Silva', '123.456.719-08', '1990-01-01', '$2a$10$pY3HxIeSPf3t4fVWHo4k/.nWKZVrJSYP/Ddc8XfZUGRcCLQfUn9iu', 'joao@example.com', 'profile1.jpg', '6ba7b810-9dad-11d1-80b4-00c04fd430c8', 'CUSTOMER'),
+('6ba7b810-9dad-11d1-80b4-00c04fd430c1', 'Maria Santos', '123.456.719-01', '1985-05-15', '$2a$10$RZFMhmzRDGHplTLaej22pO.SS8pgMOIqWMUtVVsjZLd/km1aMJIZG', 'maria@example.com', 'profile2.jpg', '6ba7b811-9dad-11d1-80b4-00c04fd430c9', 'CUSTOMER'),
+('6ba7b810-9dad-11d1-80b4-00c04fd430cb', 'Carlos Oliveira', '123.456.719-00', '1995-09-30', '$2a$10$fRIzNYEuea3hX.dICXfDzuvI/cNP6Yvw4WU8xUcxqubmRzD75efTi', 'carlos@example.com', 'profile3.jpg', '6ba7b812-9dad-11d1-80b4-00c04fd430ca', 'ADMIN');
