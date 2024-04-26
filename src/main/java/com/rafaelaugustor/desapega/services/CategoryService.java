3e78287530cd4a94b684dd0f24b3a4faddeefdfb
Rafael <rafaelaugustodev@gmail.com>
2024-04-25 22:47:47 -0300

    feat: adding method to create a new category

---
+++ src/main/java/com/rafaelaugustor/desapega/services/CategoryService.java
@@ -0,0 +1,32 @@
+package com.rafaelaugustor.desapega.services;
+
+import com.rafaelaugustor.desapega.domain.entities.Category;
+import com.rafaelaugustor.desapega.repositories.CategoryRepository;
+import com.rafaelaugustor.desapega.rest.dtos.request.CategoryRequestDTO;
+import com.rafaelaugustor.desapega.rest.dtos.response.CategoryResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository repository;
+
+    public void createCategory(CategoryRequestDTO request){
+
+        Category categoryToSave = Category.builder()
+                .name(request.getName())
+                .description(request.getDescription())
+                .image(request.getImage())
+                .build();
+
+        repository.save(categoryToSave);
+
+    }
+
+
+}
