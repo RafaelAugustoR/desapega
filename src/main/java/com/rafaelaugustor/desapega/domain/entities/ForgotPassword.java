2f0129e9178d5ee04d52da071c5ffb08457da315
Rafael <rafaelaugustodev@gmail.com>
2024-04-30 20:49:39 -0300

    feat: creating entity ForgotPassword

---
+++ src/main/java/com/rafaelaugustor/desapega/domain/entities/ForgotPassword.java
@@ -0,0 +1,31 @@
+package com.rafaelaugustor.desapega.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class ForgotPassword {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false)
+    private Integer otp;
+
+    @Column(nullable = false)
+    private Date expirationTime;
+
+    @OneToOne
+    private User user;
+}
