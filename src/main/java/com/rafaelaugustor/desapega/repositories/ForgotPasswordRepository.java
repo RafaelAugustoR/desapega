e8fa36c53caa46848a682b6ce811673af29421fa
Rafael <rafaelaugustodev@gmail.com>
2024-05-01 18:49:52 -0300

    feat: adding JPQL @Query to findByOtpAndUser

--- src/main/java/com/rafaelaugustor/desapega/repositories/ForgotPasswordRepository.java
+++ src/main/java/com/rafaelaugustor/desapega/repositories/ForgotPasswordRepository.java
@@ -1,11 +1,21 @@
 package com.rafaelaugustor.desapega.repositories;
 
 import com.rafaelaugustor.desapega.domain.entities.ForgotPassword;
+import com.rafaelaugustor.desapega.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ForgotPasswordRepository extends JpaRepository<ForgotPassword, UUID> {
+
+    @Query(
+            """
+                    SELECT fp FROM ForgotPassword fp WHERE fp.otp = ?1 AND fp.user = ?2
+                    """
+    )
+    Optional<ForgotPassword> findByOtpAndUser(Integer otp, User user);
 }
