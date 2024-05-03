bedea9871aac8e445c50e8bbd2275452179c04ab
Rafael <rafaelaugustodev@gmail.com>
2024-05-02 21:15:25 -0300

    feat: implementing method to make the user confirm his email after register

--- src/main/java/com/rafaelaugustor/desapega/services/AuthService.java
+++ src/main/java/com/rafaelaugustor/desapega/services/AuthService.java
@@ -3,8 +3,10 @@ package com.rafaelaugustor.desapega.services;
 import com.rafaelaugustor.desapega.domain.entities.Address;
 import com.rafaelaugustor.desapega.domain.entities.User;
 import com.rafaelaugustor.desapega.domain.enums.UserRole;
+import com.rafaelaugustor.desapega.domain.enums.UserStatus;
 import com.rafaelaugustor.desapega.repositories.AddressRepository;
 import com.rafaelaugustor.desapega.repositories.UserRepository;
+import com.rafaelaugustor.desapega.rest.dtos.request.EmailRequestDTO;
 import com.rafaelaugustor.desapega.rest.dtos.request.LoginRequestDTO;
 import com.rafaelaugustor.desapega.rest.dtos.request.RegisterRequestDTO;
 import com.rafaelaugustor.desapega.rest.dtos.response.LoginResponseDTO;
@@ -24,7 +26,10 @@ public class AuthService {
 
     private final AddressRepository addressRepository;
 
+    private final EmailService emailService;
+
     private final PasswordEncoder passwordEncoder;
+
     private final AuthenticationManager authenticationManager;
 
     private final TokenService tokenService;
@@ -33,6 +38,11 @@ public class AuthService {
         UsernamePasswordAuthenticationToken usernamePassword = new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword());
 
         var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        User user = (User) auth.getPrincipal();
+        if (user.getStatus() != UserStatus.ACTIVE) {
+            throw new RuntimeException("User account is pending confirmation. Please confirm your email to activate your account.");
+        }
 
         var token = tokenService.generateToken((User) auth.getPrincipal());
 
@@ -68,9 +78,27 @@ public class AuthService {
                 .address(userAddress)
                 .password(passwordEncoder.encode(request.getPassword()))
                 .role(UserRole.CUSTOMER)
+                .status(UserStatus.PENDING)
                 .build();
 
         userRepository.save(userToSave);
+
+        EmailRequestDTO confirmUserEmail = EmailRequestDTO.builder()
+                .to(request.getEmail())
+                .subject("Desapega - confirm your email")
+                .text("This is the link to confirm your email and complete your registration! https://desapega.com.br/confirm-email/" + request.getEmail().toLowerCase())
+                .build();
+        emailService.sendEmail(confirmUserEmail);
+    }
+
+    public void confirmEmail(String email){
+
+        User user = userRepository.findByEmail(email);
+
+        user.setStatus(UserStatus.ACTIVE);
+
+        userRepository.save(user);
+
     }
 
 }
